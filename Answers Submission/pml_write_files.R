5eca8ab68c62aba79ac2c9bd35b788e1946f84ef
Chanter88 <alan.gz.li@msn.cn>
2015-06-21 01:34:45 +0800

    bag

---
+++ Answers Submission/pml_write_files.R
@@ -0,0 +1,10 @@
+
+#setwd("C:/Users/Alan GZ Li/Documents/GitHub/Practical-Machine-Learning/Answers Submission")
+
+pml_write_files = function(x){
+        n = length(x)
+        for(i in 1:n){
+                filename = paste0("problem_id_",i,".txt")
+                write.table(x[i],file=filename,quote=FALSE,row.names=FALSE,col.names=FALSE)
+        }
+}
